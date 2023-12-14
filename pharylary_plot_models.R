9f6434875de06acd22bde9795cc8c0c2a1bccd4d
Benjamin Lang <blang@ucsd.edu>
2023-12-14 10:30:17 -0800

    means plots in R

--- pharylary_plot_models.R
+++ pharylary_plot_models.R
@@ -19,59 +19,276 @@ library(lme4)
 # library(janitor)
 #library(data.table)
 # library(psyphy)
-library("cowplot")
-library("forcats")
-library("lme4")
-library("optimx")
-library("rlang")
-library("scales")
+# library("cowplot")
+# library("forcats")
+# library("lme4")
+# library("optimx")
+# library("rlang")
+# library("scales")
 library("splines")
 library("stringr")
 library("tidyverse")
+library("devtools")
+library(gridExtra)
+library(grid)
 
-ms_colors <- c(
-  "ħ-V"    = "#1b9e77", # orange
-  "h-V" = "#d95f02", # blue
-  "ʔ-V" = "#7570b3",  # green
-  "ʕ-V" = "#e7298a",  # red
-  "V-ħ-V"    = "#1b9e77", # orange
-  "V-h-V" = "#d95f02", # blue
-  "V-ʔ-V" = "#7570b3",  # green
-  "V-ʕ-V" = "#e7298a",  # red
-  "V-ħ"    = "#1b9e77", # orange
-  "V-h" = "#d95f02", # blue
-  "V-ʔ" = "#7570b3",  # green
-  "V-ʕ" = "#e7298a"  # red
-)
+# ms_colors <- c(
+#   "ħ-V"    = "#1b9e77", # orange
+#   "h-V" = "#d95f02", # blue
+#   "ʔ-V" = "#7570b3",  # green
+#   "ʕ-V" = "#e7298a",  # red
+#   "V-ħ-V"    = "#1b9e77", # orange
+#   "V-h-V" = "#d95f02", # blue
+#   "V-ʔ-V" = "#7570b3",  # green
+#   "V-ʕ-V" = "#e7298a",  # red
+#   "V-ħ"    = "#1b9e77", # orange
+#   "V-h" = "#d95f02", # blue
+#   "V-ʔ" = "#7570b3",  # green
+#   "V-ʕ" = "#e7298a"  # red
+# )
 
-ms_facets <- list(
-  scale_color_manual(name = NULL, values = ms_colors),
-  scale_fill_manual(name = NULL, values = ms_colors),
-  theme_light(),
-  theme(
-    aspect.ratio     = 1,
-    legend.position  = "bottom",
-    strip.background = element_blank(),
-    strip.text       = element_text(color = "black", hjust = 0, size = 11),
-    panel.border     = element_rect(color = "black", fill = NA)
-  )
-)
+# ms_colors <- c(
+#   "ħ"    = "#1b9e77", # orange
+#   "h" = "#d95f02", # blue
+#   "ʔ" = "#7570b3",  # green
+#   "ʕ" = "#e7298a",  # red
+# )
+
+# ms_facets <- list(
+#   scale_color_manual(name = NULL, values = ms_colors),
+#   scale_fill_manual(name = NULL, values = ms_colors),
+#   theme_light(),
+#   theme(
+#     aspect.ratio     = 1,
+#     legend.position  = "bottom",
+#     strip.background = element_blank(),
+#     strip.text       = element_text(color = "black", hjust = 0, size = 11),
+#     panel.border     = element_rect(color = "black", fill = NA)
+#   )
+# )
 
 # data_path <- sprintf('/Volumes/circe/vs/output_preproc/preproc_output.csv')
-data_path <- sprintf('/Users/bcl/Desktop/preproc_output.csv')
+# data_path <- sprintf('/Users/bcl/Desktop/preproc_output.csv')
+data_path <- sprintf('/Volumes/circe/vs/output_preproc/preproc_matchesformeans.csv')
 data = read.csv(data_path)
 
+subset = subset(data, interval == 'ħ' | interval == 'h' | interval == 'ʔ' | interval == 'ʕ')
+# subset = subset(subset, Contrast_.IPA. == 'h - ħ' | Contrast_.IPA. == 'ʔ - ʕ')
+subset <- subset %>%
+  mutate(facet_contrast = ifelse(grepl("h|ħ", interval), "vcl", "v"))
+
+subset_mean <- subset %>% group_by(phrase,interval) %>% mutate(H1H2c_mean = mean(H1H2c, na.rm = TRUE))
+subset_mean <- subset_mean %>% group_by(phrase,interval) %>% mutate(CPP_mean = mean(CPP, na.rm = TRUE))
+subset_mean <- subset_mean %>% group_by(phrase,interval) %>% mutate(soe_mean = mean(soe, na.rm = TRUE))
+subset_mean <- subset_mean %>% group_by(phrase,interval) %>% mutate(sF1_mean = mean(sF1, na.rm = TRUE))
+subset_mean <- subset_mean %>% group_by(phrase,interval) %>% mutate(sF2_mean = mean(sF2, na.rm = TRUE))
+
+unique_data <- subset_mean %>% group_by(phrase,interval) %>% summarize(
+  H1H2c_mean_unique = first(H1H2c_mean),
+  CPP_mean_unique = first(CPP_mean),
+  soe_mean_unique = first(soe_mean),
+  sF1_mean_unique = first(sF1_mean),
+  sF2_mean_unique = first(sF2_mean),
+  facet_contrast = first(facet_contrast),
+  .groups = 'drop'  # This drops the grouping, so the data is no longer grouped after this operation
+)
+
+write.csv(subset_mean, "/Volumes/circe/vs/output_preproc/subset_mean.csv", row.names=FALSE)
+
+rain_height <- .1
+
+#### H1*-H2* ####
+plot1 <- ggplot(unique_data, aes(x = "", y = H1H2c_mean_unique, fill = interval)) +
+  # clouds
+  introdataviz::geom_flat_violin(trim=FALSE, alpha = 0.4,
+                                 position = position_nudge(x = rain_height+.05)) +
+  # rain
+  geom_point(aes(colour = interval), size = 2, alpha = .5, show.legend = FALSE, 
+             position = position_jitter(width = rain_height, height = 0)) +
+  # boxplots
+  geom_boxplot(width = rain_height, alpha = 0.4, show.legend = FALSE, 
+               outlier.shape = NA,
+               position = position_nudge(x = -rain_height*2)) +
+  # coord_flip() +
+  # mean and SE point in the cloud
+  # stat_summary(fun.data = mean_cl_normal, mapping = aes(color = interval), show.legend = FALSE,
+  #              position = position_nudge(x = rain_height * 3)) +
+  # adjust layout
+  scale_x_discrete(name = "", expand = c(rain_height*3, 0, 0, 0.7)) +
+  scale_y_continuous(name = "H1*-H2* (dB)",
+                     # breaks = seq(-30, 2, 30), 
+                     # limits = c(-30, 30)) +
+  ) +
+  coord_flip() +
+  facet_wrap(~factor(facet_contrast, 
+                     levels = c("vcl", "v"), 
+                     labels = c("h - ħ", "ʔ - ʕ")), 
+             nrow = 2) +
+  # custom colours and theme
+  scale_fill_brewer(palette = "Dark2", name = "Contrast Type") +
+  scale_colour_brewer(palette = "Dark2") +
+  theme_minimal() +
+  theme(panel.grid.major.y = element_blank(),
+        legend.position = c(0.9, 0.9),
+        legend.background = element_rect(fill = "white", color = "white"))
 
 
 
+##### CPP ####
+rain_height <- .1
+
+plot2 <- ggplot(unique_data, aes(x = "", y = CPP_mean_unique, fill = interval)) +
+  # clouds
+  introdataviz::geom_flat_violin(trim=FALSE, alpha = 0.4,
+                                 position = position_nudge(x = rain_height+.05)) +
+  # rain
+  geom_point(aes(colour = interval), size = 2, alpha = .5, show.legend = FALSE, 
+             position = position_jitter(width = rain_height, height = 0)) +
+  # boxplots
+  geom_boxplot(width = rain_height, alpha = 0.4, show.legend = FALSE, 
+               outlier.shape = NA,
+               position = position_nudge(x = -rain_height*2)) +
+  # coord_flip() +
+  # mean and SE point in the cloud
+  # stat_summary(fun.data = mean_cl_normal, mapping = aes(color = interval), show.legend = FALSE,
+  #              position = position_nudge(x = rain_height * 3)) +
+  # adjust layout
+  scale_x_discrete(name = "", expand = c(rain_height*3, 0, 0, 0.7)) +
+  scale_y_continuous(name = "CPP (dB)",
+                     # breaks = seq(-30, 2, 30), 
+                     # limits = c(-30, 30)) +
+  ) +
+  coord_flip() +
+  facet_wrap(~factor(facet_contrast, 
+                     levels = c("vcl", "v"), 
+                     labels = c("h - ħ", "ʔ - ʕ")), 
+             nrow = 2) +
+  # custom colours and theme
+  scale_fill_brewer(palette = "Dark2", name = "Contrast Type") +
+  scale_colour_brewer(palette = "Dark2") +
+  theme_minimal() +
+  theme(panel.grid.major.y = element_blank(),
+        legend.position = c(0.9, 0.9),
+        legend.background = element_rect(fill = "white", color = "white"))
+
+##### SoE ####
+rain_height <- .1
+
+plot3 <- ggplot(unique_data, aes(x = "", y = soe_mean_unique, fill = interval)) +
+  # clouds
+  introdataviz::geom_flat_violin(trim=FALSE, alpha = 0.4,
+                                 position = position_nudge(x = rain_height+.05)) +
+  # rain
+  geom_point(aes(colour = interval), size = 2, alpha = .5, show.legend = FALSE, 
+             position = position_jitter(width = rain_height, height = 0)) +
+  # boxplots
+  geom_boxplot(width = rain_height, alpha = 0.4, show.legend = FALSE, 
+               outlier.shape = NA,
+               position = position_nudge(x = -rain_height*2)) +
+  # coord_flip() +
+  # mean and SE point in the cloud
+  # stat_summary(fun.data = mean_cl_normal, mapping = aes(color = interval), show.legend = FALSE,
+  #              position = position_nudge(x = rain_height * 3)) +
+  # adjust layout
+  scale_x_discrete(name = "", expand = c(rain_height*3, 0, 0, 0.7)) +
+  scale_y_continuous(name = "SoE (dB)",
+                     # breaks = seq(0, 0.1, 1),
+                     # limits = c(-0.1, 1.1)) +
+                     ) +
+  coord_flip() +
+  facet_wrap(~factor(facet_contrast, 
+                     levels = c("vcl", "v"), 
+                     labels = c("h - ħ", "ʔ - ʕ")), 
+             nrow = 2) +
+  # custom colours and theme
+  scale_fill_brewer(palette = "Dark2", name = "Contrast Type") +
+  scale_colour_brewer(palette = "Dark2") +
+  theme_minimal() +
+  theme(panel.grid.major.y = element_blank(),
+        legend.position = c(0.9, 0.9),
+        legend.background = element_rect(fill = "white", color = "white"))
+
+##### F1 ####
+rain_height <- .1
+
+plot4 <- ggplot(unique_data, aes(x = "", y = sF1_mean_unique, fill = interval)) +
+  # clouds
+  introdataviz::geom_flat_violin(trim=FALSE, alpha = 0.4,
+                                 position = position_nudge(x = rain_height+.05)) +
+  # rain
+  geom_point(aes(colour = interval), size = 2, alpha = .5, show.legend = FALSE, 
+             position = position_jitter(width = rain_height, height = 0)) +
+  # boxplots
+  geom_boxplot(width = rain_height, alpha = 0.4, show.legend = FALSE, 
+               outlier.shape = NA,
+               position = position_nudge(x = -rain_height*2)) +
+  # coord_flip() +
+  # mean and SE point in the cloud
+  # stat_summary(fun.data = mean_cl_normal, mapping = aes(color = interval), show.legend = FALSE,
+  #              position = position_nudge(x = rain_height * 3)) +
+  # adjust layout
+  scale_x_discrete(name = "", expand = c(rain_height*3, 0, 0, 0.7)) +
+  scale_y_continuous(name = "F1 (Hz)",
+                     # breaks = seq(-30, 2, 30), 
+                     # limits = c(-30, 30)) +
+  ) +
+  coord_flip() +
+  facet_wrap(~factor(facet_contrast, 
+                     levels = c("vcl", "v"), 
+                     labels = c("h - ħ", "ʔ - ʕ")), 
+             nrow = 2) +
+  # custom colours and theme
+  scale_fill_brewer(palette = "Dark2", name = "Contrast Type") +
+  scale_colour_brewer(palette = "Dark2") +
+  theme_minimal() +
+  theme(panel.grid.major.y = element_blank(),
+        legend.position = c(0.9, 0.9),
+        legend.background = element_rect(fill = "white", color = "white"))
+
+##### F2 ####
+rain_height <- .1
+
+plot5 <- ggplot(unique_data, aes(x = "", y = sF2_mean_unique, fill = interval)) +
+  # clouds
+  introdataviz::geom_flat_violin(trim=FALSE, alpha = 0.4,
+                                 position = position_nudge(x = rain_height+.05)) +
+  # rain
+  geom_point(aes(colour = interval), size = 2, alpha = .5, show.legend = FALSE, 
+             position = position_jitter(width = rain_height, height = 0)) +
+  # boxplots
+  geom_boxplot(width = rain_height, alpha = 0.4, show.legend = FALSE, 
+               outlier.shape = NA,
+               position = position_nudge(x = -rain_height*2)) +
+  # coord_flip() +
+  # mean and SE point in the cloud
+  # stat_summary(fun.data = mean_cl_normal, mapping = aes(color = interval), show.legend = FALSE,
+  #              position = position_nudge(x = rain_height * 3)) +
+  # adjust layout
+  scale_x_discrete(name = "", expand = c(rain_height*3, 0, 0, 0.7)) +
+  scale_y_continuous(name = "F2 (Hz)",
+                     # breaks = seq(-30, 2, 30), 
+                     # limits = c(-30, 30)) +
+  ) +
+  coord_flip() +
+  facet_wrap(~factor(facet_contrast, 
+                     levels = c("vcl", "v"), 
+                     labels = c("h - ħ", "ʔ - ʕ")), 
+             nrow = 2) +
+  # custom colours and theme
+  scale_fill_brewer(palette = "Dark2", name = "Contrast Type") +
+  scale_colour_brewer(palette = "Dark2") +
+  theme_minimal() +
+  theme(panel.grid.major.y = element_blank(),
+        legend.position = c(0.9, 0.9),
+        legend.background = element_rect(fill = "white", color = "white"))
 
 
-
-
-
-
-
-
+grid.arrange(
+  plot1, plot2, plot3, plot4,
+  ncol = 2, nrow = 2,
+  top = "Acoustic Feature Means for Laryngeal and Pharyngeal Consonants",
+  gp=gpar(fontsize=20)
+)
 
 
 
